a16b52502594ad6e8cdaf6333e3065dfba04736f
nhutlam2802 <lamquangnhat.cm884@gmail.com>
2026-06-23 16:35:30 +0700

    Cap nhat trang chu, them components va favicon

---
+++ js/load-components.js
@@ -0,0 +1,9 @@
+async function loadComponent(id, file) {
+  const res = await fetch(file);
+  const html = await res.text();
+  document.getElementById(id).innerHTML = html;
+}
+
+loadComponent("top", "/components/top.html");
+loadComponent("header", "/components/header.html");
+loadComponent("footer", "/components/footer.html");
