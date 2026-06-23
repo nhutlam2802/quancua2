94f273700721164da924ceb795619c1b2a5f0c44
Opal0 <thaophuongtruc@gmail.com>
2026-06-23 21:57:30 +0700

    update xuly sp
    hien thi chi tiet sp

--- js/sanpham/sanpham.js
+++ js/sanpham/sanpham.js
@@ -6,15 +6,35 @@ export class Product {
         this.variants = variants;
     }
 
-    render(){ //chuyen thanh cac phan tu html cua product
+    add_thongtin(){ //chuyen thanh cac phan tu html cua product
         return `
         <div class="product">
             <img class="product__img" src="${this.image}">
-            <a href="#">${this.name}</a>
+            <a href="chitietsp.html?id=${this.id}">${this.name}</a>
             <p>${this.variants[0].price.toLocaleString('vi-VN')}đ</p>
         </div>
     `;
     }
+    thongtinsp() {
+        return `
+        <div class="product__detail">
+            <img class="product__detail__img"src="${this.image}">
+            <div class="info">
+                <h1>${this.name}</h1>
+                <label>Phân loại:</label>
+                <select id="size-select">
+                    ${this.variants.map(v =>
+                        `<option value="${v.size}">${v.size}</option>`
+                    ).join("")}
+                </select>
+                <p id="price"> ${this.variants[0].price.toLocaleString('vi-VN')}đ</p>
+                <button id="buy">Mua ngay</button>
+                <button id="add-cart">Thêm vào giỏ hàng</button>
+            </div>
+        </div>
+        `;
+    }
 }
+
 
 
--- js/sanpham/xuly.js
+++ js/sanpham/xuly.js
@@ -1,5 +1,29 @@
 import { list_product } from "./listproduct.js";
-const product = list_product.map(item=>item.render()).join("");
-//map chuyen tung ptu trong list_product tu dang object thanh html
-document.getElementById("product-list").innerHTML=product;
-//them phan tu product da chuyen doi va html
+
+const product_list = document.getElementById("product-list");
+if(product_list){
+    product_list.innerHTML=list_product.map(item=>item.add_thongtin()).join("");
+}
+let product;
+const product_detail=document.getElementById("detail");
+if (product_detail) {
+    const params = new URLSearchParams(window.location.search);
+    const id = Number(params.get("id"));
+    product = list_product.find(item => item.id === id);
+
+    if (product) {
+        product_detail.innerHTML=product.thongtinsp();
+    }
+}
+const sizeSelect = document.getElementById("size-select");
+const price = document.getElementById("price");
+
+sizeSelect.addEventListener("change", () => {
+    const variant = product.variants.find(
+        v => v.size === sizeSelect.value
+    );
+
+    price.innerText =
+        `${variant.price.toLocaleString('vi-VN')}đ`;
+});
+
