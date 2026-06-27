6a4b3de271c097ffc08e71e4ffed21ddaa7751b2
Opal0 <thaophuongtruc@gmail.com>
2026-06-27 17:11:09 +0700

    Update xuly.js
    them 1 so ham xu ly gia, add_cart,...

--- js/sanpham/xuly.js
+++ js/sanpham/xuly.js
@@ -1,29 +1,67 @@
 import { list_product } from "./listproduct.js";
-
-const product_list = document.getElementById("product-list");
-if(product_list){
+import { Item } from "./sanpham.js";
+// localStorage.clear(); 
+function khoitao_trangsp(){
+    const product_list = document.querySelector(".product-list");
+    if (!product_list) return;
     product_list.innerHTML=list_product.map(item=>item.add_thongtin()).join("");
+    
 }
-let product;
-const product_detail=document.getElementById("detail");
-if (product_detail) {
+function doigia(product,sizeSelect,price){
+    const variant = product.variant.find(v=>v.size==sizeSelect.value);
+    price.innerText = `${variant.price.toLocaleString('vi-VN')}đ`;
+}
+function khoitaotrangchitiet(){
+    const product_detail=document.querySelector(".product__detail");
+    if(!product_detail) return;
     const params = new URLSearchParams(window.location.search);
     const id = Number(params.get("id"));
-    product = list_product.find(item => item.id === id);
-
-    if (product) {
-        product_detail.innerHTML=product.thongtinsp();
-    }
+    const product = list_product.find(item => item.id == id); //so sanh tuyet doi
+        if (product) {
+            product_detail.innerHTML=product.thongtinsp();
+        }
+    const sizeSelect = document.querySelector(".product__info-select");
+    const price = document.querySelector(".product__price");
+        doigia(product,sizeSelect,price);
+    sizeSelect.addEventListener("change",function(){
+        doigia(product,sizeSelect,price)
+    });
+    add_cart(product);
 }
-const sizeSelect = document.getElementById("size-select");
-const price = document.getElementById("price");
+function inputamount(){
+    const sum = document.getElementById("sum");
+    const minus = document.getElementById("minus");
+    const input = document.querySelector(".product__count-input");
+    input.value=1;
+        sum.addEventListener("click",()=>
+        {
+            if(input.value>=1&&input.value<=1000)
+                input.value++;
+        })
+        minus.addEventListener("click",()=>
+        {
+            if(input.value>1)
+            input.value--;
+        })
+        input.addEventListener("input",()=>{
+            if (input.value>300) input.value=100;
+        })
 
-sizeSelect.addEventListener("change", () => {
-    const variant = product.variants.find(
-        v => v.size === sizeSelect.value
-    );
+}
+function add_cart(product) {
+    const input = document.querySelector(".product__count-input");
+    const add = document.getElementById("add-cart");
+    const sizeSelect = document.querySelector(".product__info-select");
+    add.addEventListener("click", () => {
+        const cart = JSON.parse(localStorage.getItem("cart")) || []; //tim du lieu luu duoi dang cart 
+    //trong localStorage, chua co thi tao 1 mang rong
+        const variant = product.variant.find(v=>v.size==sizeSelect.value);
+        const item = new Item(product,variant,input.value);
+        cart.push(item);
+        localStorage.setItem("cart", JSON.stringify(cart));     
+    });
+}
 
-    price.innerText =
-        `${variant.price.toLocaleString('vi-VN')}đ`;
-});
-
+khoitao_trangsp();
+khoitaotrangchitiet();
+inputamount();
