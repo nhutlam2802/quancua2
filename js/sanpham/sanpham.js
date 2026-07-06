a07d70cad780d63edf8ad23b4aa5d79557a3467f
Opal0 <thaophuongtruc@gmail.com>
2026-07-06 20:34:18 +0700

    chuyen ham ra khoi cau truc

--- js/sanpham/sanpham.js
+++ js/sanpham/sanpham.js
@@ -11,43 +11,6 @@ export class Product {
         this.image = image;
         this.variant = variant;
     }
-
-    add_thongtin(){ //chuyen thanh cac phan tu html cua product
-        return `
-        <div class="product">
-            <img class="product__img" src="${this.image}">
-            <a href="chitietsp.html?id=${this.id}">${this.name}</a>
-            <p>${this.variant[0].price.toLocaleString('vi-VN')}đ</p>
-        </div>
-    `;
-    }
-    thongtinsp() {
-        return `
-        <div class="product-detail">
-            <img class="product-detail__img"src="${this.image}">
-            <div class="product-detail__infor">
-                <h2>${this.name}</h2>
-                <label class="product-detail__label">Phân loại:</label>
-                <select class="product-detail__info-select">
-                    ${this.variant.map(v =>
-                        `<option value="${v.size}">${v.size}</option>`
-                    ).join("")}
-                </select>
-                <p class ="product-detail__price" > ${this.variant[0].price.toLocaleString('vi-VN')}đ</p>
-                <div class="product-detail__count">
-                    <p> Số lượng:</p>
-                    <button class="product-detail__count-btn" id="sum">+</button>
-                    <input class="product-detail__count-input" type="number">
-                    <button class="product-detail__count-btn" id = "minus">-</button>
-                </div>
-                <div class="product-detail__action">
-                    <button class="product-detail__btn" id="buy"> Mua ngay </button>
-                    <button class="product-detail__btn" id="add-cart">Thêm vào giỏ hàng</button>
-                </div>
-            </div>
-        </div>
-        `;
-    }
 }
 export class Item{
     constructor(product, variant, quantity){
