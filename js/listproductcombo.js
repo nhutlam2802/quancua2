45b462650270fc6186a74ff86ed7da501e383495
jerry-sunny393 <truongphamanhthu52@gmail.com>
2026-07-08 19:48:34 +0700

    Add files via upload

---
+++ js/listproductcombo.js
@@ -0,0 +1,41 @@
+import { Product, Variant } from "./sanpham.js";
+
+export const list_product_combo = [
+
+    new Product(
+        1,
+        "Combo 1 người",
+        "/images/combo1nguoi.png",
+        [
+            new Variant("Combo",73000)
+        ]
+    ),
+
+    new Product(
+        2,
+        "Combo 2 người",
+        "/images/combo2nguoi.png",
+        [
+            new Variant("Combo",179000)
+        ]
+    ),
+
+    new Product(
+        4,
+        "Combo 4 người",
+        "/images/combo4nguoi.png",
+        [
+            new Variant("Combo",349000)
+        ]
+    ),
+
+    new Product(
+        9,
+        "Combo 9 người",
+        "/images/combo9nguoi.png",
+        [
+            new Variant("Combo",815000)
+        ]
+    )
+
+];
