6c605846d3d64f6c5492f13b1095d1f928e4046b
Opal0 <thaophuongtruc@gmail.com>
2026-07-15 21:40:00 +0700

    bỏ phần thừa

--- js/sanpham/sanpham.js
+++ js/sanpham/sanpham.js
@@ -20,14 +20,13 @@ export class Product {
                 <img class="product__img" src="${this.image}">
                 <a href="chitietsp.html?id=${this.id}">${this.name}</a>
                 <p id="price">${this.variant[0].price.toLocaleString('vi-VN')}đ</p>
-                <p id ="rating">Đánh giá: 4.5 <span class="fa -solid fa-star"></span></p>
+                ${this.rating()}
             </div>
         `; 
         }
     //Chuyển thông tin chi tiết của sản phẩm thành một chuỗi html để hiển thị
     thongtinsp() {
             return `
-            <div class="product-detail">
                 <img class="product-detail__img"src="${this.image}">
                 <div class="product-detail__infor">
                     <h2>${this.name}</h2>
@@ -53,7 +52,23 @@ export class Product {
                         <button class="product-detail__btn" id="add-cart">Thêm vào giỏ hàng</button>
                     </div>
                 </div>
-            </div>
+            `;
+        }
+        rating(){
+            let star;
+
+            if(this.id==1 ||this.id==4 || this.id==7 || this.id==11)
+                star = 4.7;
+            else if(this.id==2 || this.id==6 || this.id==9 || this.id==12)
+                star = 4.5;
+            else
+                star = 4.0;
+//Hàm toFix(1) giữ lại 1 chữ số sau dấu phẩy, chuyển số thành dạng chuỗi
+            return `
+                <p class="rating" id ="rating">
+                    Đánh giá: ${star.toFixed(1)} 
+                    <span class="fa-solid fa-star"></span>
+                </p>
             `;
         }
 }
