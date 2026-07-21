e4d8b68ea4a0cdc81d5a966161f5476112fbe125
nhutlam2802 <lamquangnhat.cm884@gmail.com>
2026-07-22 00:55:53 +0700

    Add comment for load-components and menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,4 @@
+// Khởi tạo chức năng đóng/mở Menu trên Mobile
 export function initMenu() {
     const menuBtn = document.querySelector(".menu-toggle");
     const navbar = document.querySelector(".page-header__navbar");
