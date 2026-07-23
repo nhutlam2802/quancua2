ac6aef64797aae4f8f878cc41c08df02ebe476ad
Opal0 <thaophuongtruc@gmail.com>
2026-07-23 22:09:03 +0700

    Update listproduct.js

--- js/sanpham/listproduct.js
+++ js/sanpham/listproduct.js
@@ -1,53 +1,63 @@
-export const listproduct={
-    "1": {  "name" :"Chân gà sốt thái",
-            "img" :"/images/changa.png",
-            "variant":[{"size":"S","price":"35000"},{"size":"M","price":"50000"},{"size":"L","price":"80000"}]
+export const listproduct=[
+    {       id:1,
+            name :"Chân gà sốt thái",
+            img :"/images/changa.png",
+            variant: [{size:"S",price:"35000"},{size:"M",price:"50000"},{size:"L",price:"80000"}]
+    },
+     {      id:2,
+            name :"Bánh tráng nướng",
+            img :"/images/banhtrangnuong.png",
+            variant:[{size:"Phần",price:"20000"}]
         },
-    "2": {  "name" :"Bánh tráng nướng",
-            "img" :"/images/banhtrangnuong.png",
-            "variant":[{"size":"Phần","price":"20000"}]
+     {      id:3,
+            name :"Bánh mì nướng",
+            img :"/images/banhmi.png",
+            variant:[{size:"Phần",price:"20000"}]
         },
-    "3": {  "name" :"Bánh mì nướng",
-            "img" :"/images/banhmi.png",
-            "variant":[{"size":"Phần","price":"20000"}]
+     {      id:4,
+            name :"Mì trộn",
+            img :"/images/mitron.png",
+            variant:[{size:"S",price:"25000"},{size:"M",price:"40000"}]
         },
-    "4": {  "name" :"Mì trộn",
-            "img" :"/images/mitron.png",
-            "variant":[{"size":"S","price":"25000"},{"size":"M","price":"40000"}]
+    {       id:5,
+            name :"Bánh tráng trộn",
+            img :"/images/banhtrangtron.png",
+            variant:[{size:"Phần",price:"20000"}]
         },
-    "5": {  "name" :"Bánh tráng trộn",
-            "img" :"/images/banhtrangtron.png",
-            "variant":[{"size":"Phần","price":"20000"}]
+    {       id:6,
+            name :"Gà rán",
+            img :"/images/garan.png",
+            variant:[{size:"Phần",price:"35000"}]
         },
-    "6": {  "name" :"Gà rán",
-            "img" :"/images/garan.png",
-            "variant":[{"size":"Phần","price":"35000"}]
+     {      id:7,
+            name :"Xúc xích chiên xù",
+            img :"/images/xucxich.png",
+            variant:[{size:"Phần",price:"10000"}]
         },
-    "7": {  "name" :"Xúc xích chiên xù",
-            "img" :"/images/xucxich.png",
-            "variant":[{"size":"Phần","price":"10000"}]
+     {      id:8,
+            name :"Khoai tây chiên",
+            img :"/images/khoaichien.png",
+            variant:[{size:"S",price:"10000"},{size:"M",price:"15000"}]
         },
-    "8": {  "name" :"Khoai tây chiên",
-            "img" :"/images/khoaichien.png",
-            "variant":[{"size":"S","price":"10000"},{"size":"M","price":"15000"}]
+     {      id:9,
+            name :"Bắp xào",
+            img :"/images/bapxao.png",
+            variant:[{size:"Phần",price:"15000"}]
         },
-    "9": {  "name" :"Bắp xào",
-            "img" :"/images/bapxao.png",
-            "variant":[{"size":"Phần","price":"15000"}]
+     {      id:10,
+            name :"Chả giò",
+            img :"/images/chagio.png",
+            variant:[{size:"Phần",price:"12000"}]
         },
-    "10": { "name" :"Chả giò",
-            "img" :"/images/chagio.png",
-            "variant":[{"size":"Phần","price":"12000"}]
+     {      id:11,
+            name :"Gà lắc phô mai",
+            img :"/images/galac.png",
+            variant:[{size:"Phần",price:"30000"}]
         },
-    "11": { "name" :"Gà lắc phô mai",
-            "img" :"/images/galac.png",
-            "variant":[{"size":"Phần","price":"30000"}]
-        },
-    "12": { "name" :"Xoài lắc",
-            "img" :"/images/xoailac.png",
-            "variant":[{"size":"Phần","price":"10000"}]
+     {      id:12,
+            name :"Xoài lắc",
+            img :"/images/xoailac.png",
+            variant:[{size:"Phần",price:"10000"}]
         }
-        
     
-}
-    
+]
